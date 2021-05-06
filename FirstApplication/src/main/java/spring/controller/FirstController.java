fab99568a87beea2c057c83ca42f1a3b3c3168ba
ankit <ankit@LAPTOP-1SBVFVLI>
2021-05-06 11:35:55 +0530

    frst application

---
+++ FirstApplication/src/main/java/spring/controller/FirstController.java
@@ -0,0 +1,15 @@
+package spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FirstController {
+	@RequestMapping("/")
+	public String processRequest() {
+		
+		return "Second";
+		
+	}
+
+}
